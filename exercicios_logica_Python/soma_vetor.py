93ba44f4e58272c5a376048c8fe438698acbd9f3
Caroline <2015302@aluno.univesp.br>
2022-07-20 22:11:24 -0300

    soma vetor

---
+++ exercicios_logica_Python/soma_vetor.py
@@ -0,0 +1,19 @@
+#problema "soma vetor"
+N = int(input("Quantos números você vai digitar? "))
+
+vet= [0 for x in range(N)]
+for i in range(0, N):
+    vet[i] = float(input("Digite um número: "))
+print()
+print("VALORES = ",end= "")
+
+for i in range(0, N):
+    print(f"{vet[i]:.1f} ", end= "") 
+print()
+
+soma=0
+for i in range(0,N):
+    soma = soma + vet[i]
+media = soma / N
+print(f"SOMA = {soma:.2f}")
+print(f"MEDIA = {media:.2f}")
